ab2e8141a13c70d9d10f9069e6139a652b6334a7
Pablo Vim <pablo.vim@proton.me>
2024-09-29 14:35:03 +0300

    update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,50 +1,42 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- https://github.com/EmranMR/tree-sitter-blade/discussions/19
-      -- https://github.com/calebdw/dotfiles/blob/master/.config/nvim/queries/blade/injections.scm
-      vim.list_extend(opts.ensure_installed, {
-        "php",
-        "php_only",
-      })
-
-      opts.endwise = {
+    opts = {
+      endwise = {
         enable = true,
-      }
-      opts.swap = {
-        enable = true,
-      }
-
-      local tbl_filetypes = require("nvim-ts-autotag.internal").tbl_filetypes
-      opts.autotag = {
-        enable = true,
-        filetypes = vim.list_extend(tbl_filetypes, { "blade" }),
-      }
-
-      opts.select = {
-        enable = true,
-      }
-      opts.textobjects = {
-        lsp_interop = {
-          enable = true,
-        },
-      }
-
-      -- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-      -- parser_config.blade = {
-      --   install_info = {
-      --     url = "https://github.com/EmranMR/tree-sitter-blade",
-      --     files = { "src/parser.c" },
-      --     branch = "main",
-      --     generate_requires_npm = true,
-      --     requires_generate_from_grammar = true,
+      },
+      -- textobjects = {
+      --   swap = {
+      --     enable = true,
+      --     swap_next = {
+      --       ["<leader>a"] = "@parameter.inner",
+      --     },
+      --     swap_previous = {
+      --       ["<leader>A"] = "@parameter.inner",
+      --     },
       --   },
-      --   filetype = "blade",
-      -- }
-
-      -- require("nvim-treesitter.configs").setup(opts)
-    end,
+      -- },
+    },
+    -- opts = function(_, opts)
+    -- opts.endwise = {
+    --   enable = true,
+    -- }
+    -- opts.swap = {
+    --   enable = true,
+    -- }
+    --
+    -- opts.autotag = {
+    --   enable = true,
+    -- }
+    --
+    -- opts.select = {
+    --   enable = true,
+    -- }
+    -- opts.textobjects = {
+    --   lsp_interop = {
+    --     enable = true,
+    --   },
+    -- }
+    -- end,
   },
 }
