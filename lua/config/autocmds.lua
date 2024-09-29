27309a3282d03bf36e440ea6986cc27fe96d02ce
Pablo Vim <pablo.vim@proton.me>
2024-09-29 13:06:30 +0300

    move cursorliine and vertical line config to autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,3 +2,5 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.g.blamer_enabled = true
+vim.opt.colorcolumn = "120" -- show vertical line
+vim.opt.cursorline = true
