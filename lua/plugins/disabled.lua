99026435d1341a73e6612dc894184c1f7148f10a
Pablo Vim <pablo.vim@proton.me>
2024-09-29 14:34:20 +0300

    dasable alpha

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
+  { "goolord/alpha-nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "folke/flash.nvim", enabled = false },
